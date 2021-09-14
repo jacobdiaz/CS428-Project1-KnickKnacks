04fed49f7c9fde9889f792e02dca9b7c2d29525e
Jacob Diaz <hello@jacobdiaz.dev>
2021-09-14 15:29:35 -0500

    First commit,project and .gitignore

---
+++ Assets/changeEightBall.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class changeEightBall : MonoBehaviour
+{
+    public GameObject eightBallTextObject;
+    string [] responses = {
+        // Positive
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes.",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        // Unclear
+        "Reply hazy, try again",
+        "Ask again later",
+        "Better not tell you now",        
+        "Cannot predict now",
+        "Concentrate and ask again",
+        // Negative
+        "Don't count on it",        
+        "Don't count on it",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+        };
+
+
+         static System.Random random = new System.Random(); 
+         int randomResponse = random.Next(19);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            InvokeRepeating("Update", 0f, 10f);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+         eightBallTextObject.GetComponent<TextMeshPro>().text = responses[randomResponse];   
+    }
+}
